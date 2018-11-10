35396f7ff9250be19f95df0c13700a263c98aa32
马远征 <myz1104@126.com>
2018-11-10 17:03:11 +0800

    mars with xlog

---
+++ mars.framework/Headers/comm/verinfo.h
@@ -0,0 +1,11 @@
+
+#ifndef Mars_verinfo_h
+#define Mars_verinfo_h
+
+#define MARS_REVISION "4396e75"
+#define MARS_PATH "master"
+#define MARS_URL ""
+#define MARS_BUILD_TIME "2018-11-10 16:44:43"
+#define MARS_TAG "zx_[2018-11-10_16.44]@git-4396e75@myz"
+
+#endif
